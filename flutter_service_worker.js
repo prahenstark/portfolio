5db1b8bc0b39a594408235c35883e5d424d9acc4
prahenstark <parijaprahen@gmail.com>
2021-08-11 01:45:27 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -45,8 +45,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3695068652cbae6f5ad9a9a1d35b1f7a",
-"/": "3695068652cbae6f5ad9a9a1d35b1f7a",
+"index.html": "412e0c9a76251232402ccfabf600d16e",
+"/": "412e0c9a76251232402ccfabf600d16e",
 "main.dart.js": "be95e40d04b77b1c9ddfe0a02bcb77ec",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
